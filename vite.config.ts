854f362401fd53c77c82bd55a061b34f9de60ee3
Iván Gk <istng@users.noreply.github.com>
2025-06-03 11:52:05 -0300

    update vite config and build script (#24)

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,18 @@ import checker from 'vite-plugin-checker'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      supported: {
+        'top-level-await': true,
+      },
+    },
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true,
+    },
+  },
   plugins: [
     react(),
     checker({
